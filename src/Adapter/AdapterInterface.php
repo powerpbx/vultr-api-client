0e17c382354f1b2e34f579530326ee444e847209
malc0mn <malc0mn@users.noreply.github.com>
2016-02-13 16:38:02 +0100

    Updated AdapterInterface docs.

--- src/Adapter/AdapterInterface.php
+++ src/Adapter/AdapterInterface.php
@@ -20,7 +20,7 @@ interface AdapterInterface
      * @param string $url  API method to call
      * @param array  $args Argument to pas along with the method call.
      *
-     * @return mixed
+     * @return array
      */
     public function get($url, array $args = []);
 
@@ -30,6 +30,9 @@ interface AdapterInterface
      * @param string  $url     API method to call
      * @param array   $args    Argument to pas along with the method call.
      * @param boolean $getCode whether or not to return the HTTP response code.
+     *
+     * @return array|integer when $getCode is set, the HTTP response code will
+     * be returned, otherwise an array of results will be returned.
      */
     public function post($url, array $args, $getCode = false);
 }
